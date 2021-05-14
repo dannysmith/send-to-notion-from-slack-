768ba5dfdbe2285e006fb9bf396bbea9dd16c16b
Glitch (hello-express) <none>
2021-05-14 21:34:12 +0000

    🐫✍️ Checkpoint ./app.js:1407078/697

--- app.js
+++ app.js
@@ -10,31 +10,12 @@ app.command('/helloworld', async ({ ack, payload, context }) => {
   // Acknowledge the command request
   ack();
 
+  // Respond with a canned message
   try {
     const result = await app.client.chat.postMessage({
       token: context.botToken,
-      // Channel to send message to
       channel: payload.channel_id,
-      // Include a button in the message (or whatever blocks you want!)
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: 'Go ahead. Click it.'
-          },
-          accessory: {
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: 'Click me!'
-            },
-            action_id: 'button_abc'
-          }
-        }
-      ],
-      // Text in the notification
-      text: 'Message from Test App'
+      text: 'Message from Oyster Notion Bot'
     });
     console.log(result);
   }
