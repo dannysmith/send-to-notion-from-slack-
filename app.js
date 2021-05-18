68661bc91f367074980a8ca53ed66021c49cb77e
Glitch (hello-express) <none>
2021-05-18 23:35:04 +0000

    🐷👕 Checkpoint ./app.js:1407078/2321 ./package.json:1407078/37

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// Require the Bolt package (github.com/slackapi/bolt)
 const { App, LogLevel } = require("@slack/bolt");
+const { Client } = require('@notionhq/client');
+
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -7,85 +8,23 @@ const app = new App({
   logLevel: LogLevel.DEBUG
 });
 
-app.command('/helloworld', async ({ ack, payload, context }) => {
-  // Acknowledge the command request
-  ack();
-
-  // Respond with a canned message
-  try {
-    const result = await app.client.chat.postMessage({
-      token: context.botToken,
-      channel: payload.channel_id,
-      text: 'Message from Oyster Notion Bot!!!'
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
 });
 
-
-app.shortcut('open_modal', async ({ ack, payload, client }) => {
-  // Acknowledge shortcut request
-  ack();
+app.shortcut('create_notion_record', async ({ ack, payload, client }) => {
 
   try {
-    // Call the views.open method using the WebClient passed to listeners
-    const result = await client.views.open({
-      trigger_id: payload.trigger_id,
-      view: {
-        "type": "modal",
-        "title": {
-          "type": "plain_text",
-          "text": "My App"
-        },
-        "close": {
-          "type": "plain_text",
-          "text": "Close"
-        },
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-            }
-          },
-          {
-            "type": "context",
-            "elements": [
-              {
-                "type": "mrkdwn",
-                "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-              }
-            ]
-          }
-        ]
-      }
-    });
+    // Acknowledge shortcut request
+    await ack();
 
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-// All the room in the world for your code
-// app.shortcut('create_notion_record', async ({ shortcut, ack, client }) => {
-
-//   try {
-//     // Acknowledge shortcut request
-//     await ack();
-
-//     console.log(shortcut);
+    console.log(payload);
     
-//   }
-//   catch (error) {
-//     console.error(error);
-//   }
-// });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 
 
 
